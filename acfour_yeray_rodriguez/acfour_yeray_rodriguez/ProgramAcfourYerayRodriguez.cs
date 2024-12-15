56127460b3ba72482f22c24212788004d6b9ed9c
YerayRodriguez <yeray.rodriguez.7e5@itb.cat>
2024-12-15 16:49:47 +0100

    GetNatNum implemented

--- acfour_yeray_rodriguez/acfour_yeray_rodriguez/ProgramAcfourYerayRodriguez.cs
+++ acfour_yeray_rodriguez/acfour_yeray_rodriguez/ProgramAcfourYerayRodriguez.cs
@@ -1,15 +1,46 @@
-﻿using System;
-
-namespace Acfour
+﻿namespace Acfour
 {
     public class ProgramAcfourYerayRodriguez
     {
+        const string AskArrayLength = "Introdueix quants numeros vols introduir";
+        const string NatNumErr = "El numero a de ser natural";
+        const string InvalidErr = "Valor no valid, ha de ser un numero natural";
+        const string LargeErr = "El numero es massa llarg";
         public static void Main()
         {
-            Console.WriteLine();
+            Console.WriteLine(AskArrayLength);
             int[] myArray = new int[GetNatNum()];
             FillArray(myArray);
             PrintArray(myArray);
+        }
+        public static int GetNatNum()
+        {
+            int userNum = 0;
+            bool flag = false;
+            while (flag)
+            {
+                try
+                {
+                    userNum = int.Parse(Console.ReadLine());
+                    if (userNum > 0 && userNum % 2 == 0)
+                    {
+                        flag = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine(NatNumErr);
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine(InvalidErr);
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine(LargeErr);
+                }
+            }
+            return userNum;
         }
     }
 }
