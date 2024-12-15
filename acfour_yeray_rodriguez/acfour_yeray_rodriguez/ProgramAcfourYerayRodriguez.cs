da27a0b71cb72cdfb91427f9991ac94348e2eb02
YerayRodriguez <yeray.rodriguez.7e5@itb.cat>
2024-12-15 16:29:07 +0100

    Base Main Completed

---
+++ acfour_yeray_rodriguez/acfour_yeray_rodriguez/ProgramAcfourYerayRodriguez.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Acfour
+{
+    public class ProgramAcfourYerayRodriguez
+    {
+        public static void Main()
+        {
+            Console.WriteLine();
+            int[] myArray = new int[GetNatNum()];
+            FillArray(myArray);
+            PrintArray(myArray);
+        }
+    }
+}
